ad23e89f7ff626e8e8ea53a9da34e5394d5fe1d3
sachindaMass <sachinda46@gmail.com>
2023-02-21 05:22:34 +0530

    Spring Security to basic Authentication.

---
+++ src/main/java/com/example/spring_security_2023/SpringSecurity2023Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity2023Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/spring_security_2023/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.spring_security_2023.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+
+}
